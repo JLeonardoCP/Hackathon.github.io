645979a82f6d8c99a7dbb72dcbeba39aa7c0ee1c
JLeonardoCP <111620142+JLeonardoCP@users.noreply.github.com>
2024-04-12 15:01:50 -0600

    initial commit

---
+++ funciones.js
@@ -0,0 +1,28 @@
+// SUBMENU PRINCIPAL
+$(".submenu").click(function () {
+  $(this).children("ul").slideToggle();
+});
+
+$("ul").click(function (p) {
+  p.stopPropagation();
+});
+
+//BOTON SUBIR
+$(document).ready(function () {
+  $(".ir-arriba").click(function () {
+    $("body, html").animate(
+      {
+        scrollTop: "0px",
+      },
+      300
+    );
+  });
+
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 0) {
+      $(".ir-arriba").slideDown(300);
+    } else {
+      $(".ir-arriba").slideUp(300);
+    }
+  });
+});
